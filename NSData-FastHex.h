166356a8651e8459ccc50ee22931785bfc7d335b
Jonathon Mah <me@JonathonMah.com>
2015-05-14 08:46:05 -0700

    Add decode implementation from Stack Overflow
    Set performance baseline.
    
    http://stackoverflow.com/a/2353456/3948

--- NSData-FastHex.h
+++ NSData-FastHex.h
@@ -14,7 +14,6 @@ NS_ASSUME_NONNULL_BEGIN
 @interface NSData (FastHex)
 
 + (instancetype)dataWithHexString:(NSString *)hexString;
-+ (instancetype)dataWithHexString:(NSString *)hexString ignoreOtherCharacters:(BOOL)ignoreOtherCharacters;
 
 - (NSString *)hexString;
 
--- NSData-FastHex.m
+++ NSData-FastHex.m
@@ -12,11 +12,21 @@
 @implementation NSData (FastHex)
 
 + (instancetype)dataWithHexString:(NSString *)hexString
-{ return [self dataWithHexString:hexString ignoreOtherCharacters:YES]; }
-
-+ (instancetype)dataWithHexString:(NSString *)hexString ignoreOtherCharacters:(BOOL)ignoreOtherCharacters
 {
-    return nil;
+    if (!hexString) // nonnull parameter for nonnull return
+        return nil;
+
+    NSMutableData *commandToSend= [[NSMutableData alloc] init];
+    unsigned char whole_byte;
+    char byte_chars[3] = {'\0','\0','\0'};
+    for (int i = 0; i < ([hexString length] / 2); i++) {
+        byte_chars[0] = [hexString characterAtIndex:i*2];
+        byte_chars[1] = [hexString characterAtIndex:i*2+1];
+        whole_byte = strtol(byte_chars, NULL, 16);
+        [commandToSend appendBytes:&whole_byte length:1];
+    }
+
+    return commandToSend;
 }
 
 - (NSString *)hexString
