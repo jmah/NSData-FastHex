470907abb23464e194739b6017c0539e97530ee0
Jonathon Mah <me@JonathonMah.com>
2015-05-14 08:34:42 -0700

    Add nullability to header

--- NSData-FastHex.h
+++ NSData-FastHex.h
@@ -9,6 +9,8 @@
 #import <Foundation/Foundation.h>
 
 
+NS_ASSUME_NONNULL_BEGIN
+
 @interface NSData (FastHex)
 
 + (instancetype)dataWithHexString:(NSString *)hexString;
@@ -17,3 +19,5 @@
 - (NSString *)hexString;
 
 @end
+
+NS_ASSUME_NONNULL_END
